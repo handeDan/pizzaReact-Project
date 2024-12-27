a8f67e3c216574d9478a6ced42cbd86008064ffc
hande <hande.dann@gmail.com>
2024-12-27 20:07:54 +0300

    npm libraries are installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import workintech from "/workintech.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
+        <a
+          href="https://github.com/Workintech/fsweb-s7-challenge-pizza"
+          target="_blank"
+        >
           <img src={workintech} className="logo" alt="Workintech logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -29,7 +33,7 @@ function App() {
         Click on the Workintech or Pizza logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
